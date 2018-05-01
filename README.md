6a21ba6032a15c3f99ab821d417b7fc7831d08da
Furkan Taha ONDER <furkantahaonder@gmail.com>
2018-05-02 00:58:28 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Crypto-Tweet
+A python bot that tweets the price and change amount of crypto-currencies.
+
+# Requirements	
+Twitter streaming API access.  
+tweepy package  
+ticker.json file(https://koineks.com/ticker) 
+
+# Setup
+1.Clone the repo  
+2.pip3 install tweepy  
+
+# Running
+python3 bot.py
+
+# Sample bots
+BTC: https://twitter.com/bitcoinTLkuru/  
+ETH: https://twitter.com/EthereumKuru/  
+LTC: https://twitter.com/LitecoinKuru/  
+
+
---
+++ bot.py
@@ -0,0 +1,88 @@
+import json
+import time
+import os
+import tweepy
+import time
+
+global time, up, down
+# local time settings
+os.environ['TZ'] = 'Turkey'
+time = time.strftime("%d-%m-%Y %H:%M")
+up = '⬆️'
+down = '⬇️'
+
+
+def BTC():
+    # BTC twitter account key and tokens
+    consumer_key = ""
+    consumer_secret = ""
+    access_token = ""
+    access_token_secret = ""
+    login = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    login.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(login)
+    
+    with open('ticker.json') as json_data:
+        data = json.load(json_data)
+        current = str(data["BTC"]["current"])
+        change_a = str(float(data["BTC"]["change_amount"]))
+        change = str(data["BTC"]["change_percentage"])
+
+        if change[0:1] == '-':
+            api.update_status(status="#BTC: "+current+"TL"+up+"(-"+change_a+")\n"+time)
+        elif change == '0':
+            api.update_status(status="#BTC: "+current+"TL"+"("+change_a+")\n"+time)
+        else:
+            api.update_status(status="#BTC: "+current+"TL"+down+"(+"+change_a+")\n"+time)
+
+
+def ETH():
+    # ETH twitter account key and tokens
+    consumer_key = ""
+    consumer_secret = ""
+    access_token = ""
+    access_token_secret = ""
+    login = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    login.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(login)
+    
+    with open('ticker.json') as json_data:
+        data = json.load(json_data)
+        current = str(data["ETH"]["current"])
+        change_a = str(float(data["ETH"]["change_amount"]))
+        change = str(data["ETH"]["change_percentage"])
+
+        if change[0:1] == '-':
+            api.update_status(status="#ETH: "+current+"TL"+up+"(-"+change_a+")\n"+time)
+        elif change == '0':
+            api.update_status(status="#ETH: "+current+"TL"+"("+change_a+")\n"+time)
+        else:
+            api.update_status(status="#ETH: "+current+"TL"+down+"(+"+change_a+")\n"+time)
+
+
+def LTC():
+    # LTC twitter account key and tokens
+    consumer_key = ""
+    consumer_secret = ""
+    access_token = ""
+    access_token_secret = ""
+    login = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    login.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(login)
+    
+    with open('ticker.json') as json_data:
+        data = json.load(json_data)
+        current = str(data["LTC"]["current"])
+        change_a = str(float(data["LTC"]["change_amount"]))
+        change = str(data["LTC"]["change_percentage"])
+
+        if change[0:1] == '-':
+            api.update_status(status="#LTC: "+current+"TL"+up+"(-"+change_a+")\n"+time)
+        elif change == '0':
+            api.update_status(status="#LTC: "+current+"TL"+"("+change_a+")\n"+time)
+        else:
+            api.update_status(status="#LTC: "+current+"TL"+down+"(+"+change_a+")\n"+time)
+
+BTC()
+ETH()
+LTC()
